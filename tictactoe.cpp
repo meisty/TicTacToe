d49c62b15daab4890d8d1a10d48e8942626bfa47
Shaun Dixon <shaundixon666@gmail.com>
2016-05-23 11:18:29 +0100

    Removal of working code
    Removed some code that was left in from planning and construction.

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -27,8 +27,8 @@ char Input()
     char a;
     cout << "Enter your choice: ";
     cin >> a;
-    
-    
+
+
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
@@ -46,53 +46,6 @@ char Input()
                 }
             }
         }
-    
-    
-    
-    /*    
-    if (a == 1)
-    {
-        if (matrix[0][0] != player)
-            matrix[0][0] = player;
-        else
-        {
-            cout << "That has already been chosen.  Please select another number" << endl;
-            e = 'y';
-            return e;
-        }
-    }
-    else if (a == 2)
-    {
-        matrix[0][1] = player;
-    }
-    else if (a == 3)
-    {
-        matrix[0][2] = player;
-    }
-    else if (a == 4)
-    {
-        matrix[1][0] = player;
-    }
-    else if (a == 5)
-    {
-        matrix[1][1] = player;
-    }
-    else if (a == 6)
-    {
-        matrix[1][2] = player;
-    }
-    else if (a == 7)
-    {
-        matrix[2][0] = player;
-    }
-    else if (a == 8)
-    {
-        matrix[2][1] = player;
-    }
-    else if (a == 9)
-    {
-        matrix[2][2] = player;
-    }*/
 
 }
 
@@ -117,38 +70,38 @@ char Win(){
         return 'X';
     if(matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X')
         return 'X';
-    
+
     if(matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X')
         return 'X';
     if(matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X')
         return 'X';
     if(matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X')
         return 'X';
-    
+
     if(matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X')
         return 'X';
     if(matrix[0][2] == 'X' && matrix[1][1] == 'X' && matrix[2][0] == 'X')
         return 'X';
-    
+
         if(matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
         return 'O';
     if(matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
         return 'O';
     if(matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O')
         return 'O';
-    
+
     if(matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O')
         return 'O';
     if(matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
         return 'O';
     if(matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O')
         return 'O';
-    
+
     if(matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O')
         return 'O';
     if(matrix[0][2] == 'O' && matrix[1][1] == 'O' && matrix[2][0] == 'O')
         return 'O';
-    
+
     return '/';
 }
 
@@ -157,7 +110,7 @@ int main()
 {
 
     Draw();
-    
+
     while(1)
     {
         Input();
@@ -177,6 +130,6 @@ int main()
         else
             e = 'n';
     }
-    
+
     return 0;
 }
